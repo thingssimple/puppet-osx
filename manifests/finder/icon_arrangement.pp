e062bc4d623bc2c6a536ce868511a52e6918f3ee
Joe Moon <joe@xoxomoon.com>
2013-07-21 16:06:20 -0700

    invalid plist setting

--- manifests/finder/icon_arrangement.pp
+++ manifests/finder/icon_arrangement.pp
@@ -10,7 +10,6 @@ class osx::finder::icon_arrangement($style = 'grid') {
     "/usr/libexec/plistbuddy -c 'Set :DesktopViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
     "/usr/libexec/plistbuddy -c 'Set :FK_StandardViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
     "/usr/libexec/plistbuddy -c 'Set :StandardViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
-    "/usr/libexec/plistbuddy -c 'Set :ComputerViewSettings:IconViewSettings:arrangeBy ${style}' /Users/${::boxen_user}/Library/Preferences/com.apple.finder.plist",
   ]:
 
     notify => [
